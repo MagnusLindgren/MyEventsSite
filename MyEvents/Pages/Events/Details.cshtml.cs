08c2e8aecac784a0d1b9d13a87062b43cf48a085
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-03-19 09:45:16 +0100

    Added Event details

---
+++ MyEvents/Pages/Events/Details.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyEvents.Data;
+using MyEvents.Models;
+
+namespace MyEvents.Pages.Events
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MyEvents.Data.MyEventsContext _context;
+
+        public DetailsModel(MyEvents.Data.MyEventsContext context)
+        {
+            _context = context;
+        }
+
+        public Event Event { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Event = await _context.Events
+                .Include(e => e.Organizer).FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Event == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
